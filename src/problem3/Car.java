e7baa70657f7f2de576485bf1c649f0df197760c
chabee <jhcha990917@gmail.com>
2025-11-30 01:02:37 +0900

    first commit

---
+++ src/problem3/Car.java
@@ -0,0 +1,62 @@
+package problem3;
+
+import problem2.CompanyName;
+import problem2.door.Door;
+import problem2.roof.Roof;
+import problem2.wheel.Wheel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ==============BEGIN==============
+ * Complete the below code that requires your implementation
+ * If necessary, you can make your own methods or classes so that the corresponding tests are passed
+ */
+public class Car {
+    private List<Door> doors;
+    private List<Wheel> wheels;
+    private Roof roof;
+    private Color color;
+    private CompanyName companyName;
+
+    public Car() {
+        this.doors = new ArrayList<>();
+        this.wheels = new ArrayList<>();
+        this.roof = null;
+        this.color = Color.BLACK;
+    }
+
+    public int numWheels() {
+        return 0;
+    }
+
+    public int numDoors() {
+        return 0;
+    }
+
+    public int numRoofs() {
+        return 0;
+    }
+
+    public Color getColor() { return null; }
+
+    public CompanyName getCompanyName() {
+        return null;
+    }
+
+    public List<Door> getDoors() {
+        return null;
+    }
+
+    public List<Wheel> getWheels() {
+        return null;
+    }
+
+    public Roof getRoof() {
+        return null;
+    }
+}
+/**
+ * ===============END===============
+ */
---
+++ test/problem1/Test.java
@@ -0,0 +1,90 @@
+package problem1;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * DO NOT MODIFY THIS CLASS
+ * - Any change will discount your score!!
+ */
+class Test {
+    @org.junit.jupiter.api.Test
+    public void test_01(){
+        /**
+         * Test on implementation of SimpleLogger
+         */
+        SimpleLogger logger = SimpleLogger.getInstance();
+        logger.clear();
+        // don't need to print a log message through the standard out (System.out)
+        // during this test procedure; thus, its output is suppressed
+        logger.setHandler(new PrintStream(OutputStream.nullOutputStream()));
+
+        int numTests = 10;
+        for(int i = 0; i < numTests; i++)
+            logger.log(LogLevel.INFO, "test");
+
+        // test if the logger correctly preserves the log messages
+        assertEquals(numTests, logger.getNumLogs());
+
+        // test if the clear function works correctly
+        logger.clear();
+        assertEquals(0, logger.getNumLogs());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void test_02(){
+        /**
+         * Test on thread-safety of SimpleLogger
+         */
+        for(int i = 0; i < 1000; i++)
+            assertTrue(parallel_run(10));
+    }
+
+    public static boolean parallel_run(int numTests){
+        /**
+         * This function tests if
+         * - 1) the instance of SimpleLogger is singleton in the multi-thread setting, and
+         * - 2) the logger correctly preserves the occurred log messages (i.e., getNumLogs() should be numTests for each trial)
+         */
+        ExecutorService service = Executors.newCachedThreadPool();
+
+        // Try to log messages over multiple threads
+        SimpleLogger[] loggers = new SimpleLogger[numTests];
+        for(int i = 0; i < numTests; i++){
+            final int idx = i;
+            service.submit(() -> {
+                loggers[idx] = SimpleLogger.getInstance();
+                loggers[idx].setHandler(new PrintStream(OutputStream.nullOutputStream()));
+                loggers[idx].log(LogLevel.INFO, String.format("This is log from [%s]-th thread", idx));
+            });
+        }
+
+        service.shutdown();
+
+        try {
+            service.awaitTermination(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            SimpleLogger logger = loggers[0];
+            // check if the logger correctly contains all messages
+            // - if multiple instances are generated, log messages are distributed over the instances
+            boolean doesContainAllMessages = logger.getNumLogs() == numTests;
+
+            // check if all references in loggers are the same
+            boolean areAllObjectsSame = true;
+            for(SimpleLogger _logger : loggers)
+                areAllObjectsSame = areAllObjectsSame && (logger == _logger);
+
+            // remove all messages for the next trial
+            logger.clear();
+
+            return doesContainAllMessages && areAllObjectsSame;
+        }
+    }
+}
